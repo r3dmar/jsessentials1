7ffcf53193f1c0ce310352e1cd24b03bc848d8c2
jaredhlukhovetsky@gmail.com <jaredhlukhovetsky@gmail.com>
2017-08-31 01:20:01 +0300

    add files with HW jsEssential in gh pages branch

---
+++ script.js
@@ -0,0 +1,34 @@
+// Tsk 1
+
+var person = "John Snow";
+var time = "16:00";
+var date = "Dec/16/1246";
+var day = "Sunday";
+var place = "DragonStone";
+var event = prompt("Do you have to meet with someone?");
+
+function resultTsk1() {
+    return (person + " have a meeting with " + event + " in " + place + " on " + day + " " + date + " at " + time);
+}
+console.log(resultTsk1());
+alert(resultTsk1());
+document.getElementById('tsk1').innerHTML=resultTsk1();
+
+// Tsk2
+
+const USD = 25.6692;
+const EUR = 30.6879;
+const EURvsUSD = 1.17;
+
+var usdInput = prompt("USD");
+var eurInput = prompt("EUR");
+
+console.log(resultMoneyCalc());
+alert(resultMoneyCalc());
+
+function resultMoneyCalc() {
+    return (eurInput + " Euros are equal " + eurInput * EUR + " UAH, "+ 
+    usdInput + " Dollars are equal " + usdInput * USD + " UAH, "  +
+    "one Euro is equal " + EURvsUSD + " Dollars.");
+}
+document.getElementById('tsk2').innerHTML=resultMoneyCalc();
