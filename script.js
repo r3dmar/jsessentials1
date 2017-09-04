a888bac08039361dc6a6a692ef0ad62665a48d7d
jaredhlukhovetsky@gmail.com <jaredhlukhovetsky@gmail.com>
2017-09-04 14:42:06 +0300

    working with js file

--- script.js
+++ script.js
@@ -17,9 +17,10 @@ const EURvsUSD = 1.17;
 
 // var usdInput = prompt ("Enter below how much USD you have", "0");
 // var eurInput = prompt("Enter below how much EUR you have", "0");
-function resultMoneyCalc() {
 var udsInput = prompt ("Enter below how much USDolars you have", "0");
 var eurInput = prompt ("Enter below how much EURO you have", "0");
+
+function resultMoneyCalc() {
 if (isNaN(+usdInput) || isNaN(+eurInput)) { 
 alert("Not a number") 
 resultMoneyCalc()
